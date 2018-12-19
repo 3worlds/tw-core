802542241c8d86e2a33017d4f81b3201b111758e
Ian Davies <u9007333@anu.edu.au>
2018-12-19 17:22:17 +1100

    issue #8

--- tw-core/scripts/fr/cnrs/iees/versioning/VersionManager.java
+++ tw-core/scripts/fr/cnrs/iees/versioning/VersionManager.java
@@ -109,7 +109,8 @@ public class VersionManager {
 	private static String[][] DEPS = { 
 			{"org.apache.commons", "commons-text", "1.+"},
 			{"fr.ens.biologie", "generics", "+"},
-			{"au.edu.anu.rscs.aot", "omugi", "0.0.3+"}
+			{"au.edu.anu.rscs.aot", "omugi", "0.0.3+"},
+			{"org.apache.ivy", "ivy", "2.2.0"}
 	};
 	// ============================================================================================
 	
