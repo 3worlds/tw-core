c92b94c2a91b1714e92ce548f66010037df41796
Ian Davies <u9007333@anu.edu.au>
2019-01-22 17:30:14 +1100

    issue #11

---
+++ tw-core/src/au/edu/anu/twcore/specificationCheck/CheckImpl.java
@@ -0,0 +1,52 @@
+/**************************************************************************
+ *  TW-CORE - 3Worlds Core classes and methods                            *
+ *                                                                        *
+ *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          * 
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  TW-CORE is a library of the principle components required by 3W       *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of TW-CORE (3Worlds Core).                          *
+ *                                                                        *
+ *  TW-CORE is free software: you can redistribute it and/or modify       *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  TW-CORE is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with TW-CORE.                                                   *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
+ *                                                                        *
+ **************************************************************************/
+
+package au.edu.anu.twcore.specificationCheck;
+
+import au.edu.anu.rscs.aot.graph.AotGraph;
+
+/**
+ * Author Ian Davies
+ *
+ * Date 22 Jan. 2019
+ */
+// not too sure this is the way to go??
+public class CheckImpl implements Checkable{
+	private AotGraph graph;
+	public CheckImpl(AotGraph graph) {
+		this.graph=graph;
+	}
+
+	@Override
+	public boolean validateGraph() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
