c922ef20624bdb84b6718b2d42aba2ff04d5514e
daviesi <u9007333@anu.edu.au>
2019-01-23 13:50:44 +1100

    Core 3w Tree label set

---
+++ tw-core/src/au/edu/anu/twcore/archetype/PrimaryTreeLabels.java
@@ -0,0 +1,21 @@
+package au.edu.anu.twcore.archetype;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import fr.cnrs.iees.twcore.constants.Configuration;
+
+public class PrimaryTreeLabels implements Configuration {
+	private static Set<String> labelSet = new HashSet<>();
+	static {
+		labelSet.add(N_ECOLOGY);
+		labelSet.add(N_CODESOURCE);
+		labelSet.add(N_DATAIO);
+		labelSet.add(N_EXPERIMENT);
+		labelSet.add(N_UI);		
+	}
+	public static boolean contains(String label) {
+		return labelSet.contains(label);
+	}
+
+}
