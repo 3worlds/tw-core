652e6158fcbdbfdd1acd8459fb895101770de56a
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-05-24 17:10:19 +0200

    refactoring archetype - doc

--- tw-core/src/fr/cnrs/iees/twcore/constants/ConfigurationNodeLabels.java
+++ tw-core/src/fr/cnrs/iees/twcore/constants/ConfigurationNodeLabels.java
@@ -19,24 +19,24 @@ public enum ConfigurationNodeLabels {
 			N_RECORD 			("record",				Object.class),
 			N_FIELD 			("field",				Object.class),
 		N_SYSTEM 				("system",				Object.class), // World
-			N_DYNAMICS 			("dynamics",			Object.class),
-				N_TIMELINE 		("timeLine",			Object.class), // timeLine
-				N_TIMEMODEL		("timeModel",			Object.class),// timeModel
-				N_EVENTQUEUE	("eventQueue",			Object.class),
-				N_PROCESS 		("process",				Object.class),
+			N_DYNAMICS 			("dynamics",			Object.class), // Simulator ?
+				N_TIMELINE 		("timeLine",			Object.class), // TimeLine
+				N_TIMEMODEL		("timeModel",			Object.class), // TimeModel
+				N_EVENTQUEUE	("eventQueue",			Object.class), // EventQueue
+				N_PROCESS 		("process",				Object.class), // ProcessNode
 				N_FUNCTION 		("function",			Object.class),
-				N_LIFECYCLE 	("lifeCycle",			Object.class),
+				N_LIFECYCLE 	("lifeCycle",			Object.class), // LifeCycle
 				N_RECRUIT 		("recruit",				Object.class),
 				N_PRODUCE 		("produce",				Object.class),
+				N_STOPPINGCONDITION("stoppingCondition",Object.class),
 			N_STRUCTURE 		("structure",			Object.class),
 				N_CATEGORYSET 	("categorySet",			Object.class),
-				N_CATEGORY 		("category",			Object.class),
-				N_COMPONENT 	("component",			Object.class),
-				N_REALISEDCOMPONENT("realisedComponent",Object.class),
+				N_CATEGORY 		("category",			Object.class), // Category
+				N_COMPONENT 	("component",			Object.class), // SystemFactory
+				N_REALISEDCOMPONENT("realisedComponent",Object.class), // ComplexSystem ?
 				N_INITIALISER 	("initialiser",			Object.class),
 				N_RELATIONTYPE 	("relationType",		Object.class),
 		N_EXPERIMENT 			("experiment",			Object.class),
-			N_STOPPINGCONDITION ("stoppingCondition",	Object.class),
 			N_DESIGN 			("design",				Object.class),
 			N_TREATMENT 		("treatment",			Object.class),
 			N_TIMEPERIOD 		("timePeriod",			Object.class),
@@ -48,7 +48,7 @@ public enum ConfigurationNodeLabels {
 			N_UITOP 			("top",					Object.class),
 			N_UIBOTTOM 			("bottom",				Object.class),
 			N_UICENTER 			("center",				Object.class),
-			N_UIWIDGET 			("widget",				Object.class),
+			N_UIWIDGET 			("widget",				Object.class), // GridNode + ElementUserInterface
 	;
 	//========================================================================================
 	private final String label;
