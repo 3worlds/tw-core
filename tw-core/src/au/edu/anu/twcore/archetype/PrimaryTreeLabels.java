12c9662f0f399b0055d0cd398356f814f047b5ba
Ian Davies <ian.davies@anu.edu.au>
2019-08-19 12:05:10 +1000

    PrimaryTree categories

--- tw-core/src/au/edu/anu/twcore/archetype/PrimaryTreeLabels.java
+++ tw-core/src/au/edu/anu/twcore/archetype/PrimaryTreeLabels.java
@@ -36,7 +36,8 @@ import fr.cnrs.iees.twcore.constants.ConfigurationNodeLabels;
 public class PrimaryTreeLabels {
 	private static Set<String> labelSet = new HashSet<>();
 	static {
-		labelSet.add(ConfigurationNodeLabels.N_SYSTEM.label());
+		labelSet.add(ConfigurationNodeLabels.N_DYNAMICS.label());
+		labelSet.add(ConfigurationNodeLabels.N_STRUCTURE.label());
 		labelSet.add(ConfigurationNodeLabels.N_DATADEFINITION.label());
 		labelSet.add(ConfigurationNodeLabels.N_EXPERIMENT.label());
 		labelSet.add(ConfigurationNodeLabels.N_UI.label());		
