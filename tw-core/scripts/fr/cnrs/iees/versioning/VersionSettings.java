79060c497e2bd8ac17cdfbfef25cf9dd532d4a90
Ian Davies <ian.davies@anu.edu.au>
2019-09-04 11:21:01 +1000

    upgrade

--- tw-core/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-core/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -84,9 +84,9 @@ public class VersionSettings {
 	protected static String[][] DEPS = { 
 			{"fr.ens.biologie", "generics", "[0.1.4,)", null},
 			{"au.edu.anu.rscs.aot", "omugi", "[0.2.4,)", null},
+			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.1,)", null},
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.2,)", null},
-			{"fr.cnrs.iees.rvgrid", "rvgrid", "[0.0.1,)", null},
-			
+			{"fr.cnrs.iees.rvgrid", "rvgrid", "[0.0.1,)", null},		
 			{"org.apache.commons", "commons-text", "1.+", null},
 			{"org.apache.ivy", "ivy", "2.2.0", null},
 			{"org.apache.odftoolkit","simple-odf","0.8.2-incubating", null},
