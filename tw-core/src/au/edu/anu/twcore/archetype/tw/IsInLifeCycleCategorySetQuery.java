085b49feb32b93fc260930c56889b50576be3528
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-09-11 10:52:44 +0200

    tyding up life cycle - checking categories in recruit and produce nodes

---
+++ tw-core/src/au/edu/anu/twcore/archetype/tw/IsInLifeCycleCategorySetQuery.java
@@ -0,0 +1,55 @@
+package au.edu.anu.twcore.archetype.tw;
+
+import au.edu.anu.rscs.aot.queries.Query;
+import au.edu.anu.twcore.ecosystem.dynamics.LifeCycle;
+import au.edu.anu.twcore.ecosystem.structure.CategorySet;
+import au.edu.anu.twcore.ecosystem.structure.Category;
+import fr.cnrs.iees.graph.Direction;
+import fr.cnrs.iees.graph.impl.TreeGraphNode;
+import static au.edu.anu.rscs.aot.queries.base.SequenceQuery.*;
+import static au.edu.anu.rscs.aot.queries.CoreQueries.*;
+import static fr.cnrs.iees.twcore.constants.ConfigurationEdgeLabels.*;
+import static fr.cnrs.iees.twcore.constants.ConfigurationNodeLabels.*;
+import java.util.List;
+
+/**
+ * A Query to check that a categorized object's categories belong to a categorySet found in
+ * its parent - designed for testing that recruit and produce nodes only deal with the categorySet
+ * applicable to their parent lifecycle 
+ * 
+ * @author Jacques Gignoux - 11 sept. 2019
+ *
+ */
+// tested, works ok. 11/9/2019
+public class IsInLifeCycleCategorySetQuery extends Query {
+
+	public IsInLifeCycleCategorySetQuery() {
+		super();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Query process(Object input) {  // input is a recruit or produce node
+		TreeGraphNode node = (TreeGraphNode) input;
+		LifeCycle parent = (LifeCycle) node.getParent();
+		CategorySet catset =  (CategorySet) get(parent.edges(Direction.OUT),
+			selectOne(hasTheLabel(E_APPLIESTO.label())),
+			endNode());
+		List<Category> cats = (List<Category>) get(catset.getChildren(),
+			selectOneOrMany(hasTheLabel(N_CATEGORY.label())));
+		Category toCat = (Category) get(node.edges(Direction.OUT),
+			selectOne(hasTheLabel(E_TOCATEGORY.label())),
+			endNode());
+		Category fromCat = (Category) get(node.edges(Direction.OUT),
+			selectOne(hasTheLabel(E_FROMCATEGORY.label())),
+			endNode());
+		satisfied = (cats.contains(fromCat) && cats.contains(toCat));
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + stateString() + " 'to' and 'from' categories must be in the life cycle category set]";
+	}
+
+}
