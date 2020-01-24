c3c64f9442172354b66865bac700986ee6fd26d6
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-01-24 17:03:29 +0100

    implementing quadtree searching of systemcomponents

--- tw-core/src/au/edu/anu/twcore/ecosystem/runtime/system/RegionComponentContainer.java
+++ tw-core/src/au/edu/anu/twcore/ecosystem/runtime/system/RegionComponentContainer.java
@@ -4,6 +4,10 @@ import au.edu.anu.twcore.data.runtime.TwData;
 import au.edu.anu.twcore.ecosystem.runtime.Categorized;
 import au.edu.anu.twcore.ecosystem.runtime.containers.IndexedContainer;
 import fr.cnrs.iees.uit.indexing.BoundedRegionIndexingTree;
+import fr.cnrs.iees.uit.space.Box;
+import fr.cnrs.iees.uit.space.Point;
+import fr.cnrs.iees.uit.space.Sphere;
+import fr.cnrs.iees.uit.space.SphereImpl;
 
 /**
  * 
@@ -20,18 +24,38 @@ public class RegionComponentContainer
 			TwData parameters, TwData variables) {
 		super(cats, proposedId, parent, parameters, variables);
 		// TODO Auto-generated constructor stub
+		// build indexer - need variables, only doubles or floats
+		Box limits = Box.boundingBox(Point.newPoint(0,0,0,0),Point.newPoint(10,10,8,12));
+		indexer = new BoundedRegionIndexingTree<>(limits);
+		for (SystemComponent sc:allItems())
+			indexer.insert(sc,coordinates(sc));
 	}
 
 	@Override
 	public SystemComponent getNearestItem(SystemComponent item) {
-		// TODO Auto-generated method stub
-		return null;
+		return indexer.getNearestItem(coordinates(item));
 	}
 
 	@Override
 	public Iterable<SystemComponent> getItemsWithin(SystemComponent item, double distance) {
-		// TODO Auto-generated method stub
-		return null;
+		Sphere itemSphere = new SphereImpl(coordinates(item),distance);
+		return indexer.getItemsWithin(itemSphere);
+	}
+	
+	private Point coordinates(SystemComponent sc) {
+		// TODO: really compute coordinates of system component here
+		return Point.newPoint(0);
+	}
+
+	@Override
+	public void effectChanges() {
+		for (String id : itemsToRemove) {
+			SystemComponent sc = items.get(id);
+			indexer.remove(sc,coordinates(sc));
+		}
+		for (SystemComponent sc: itemsToAdd)
+			indexer.insert(sc,coordinates(sc));
+		super.effectChanges();
 	}
 
 }
