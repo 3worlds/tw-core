0f67f649e385f52bdfa922fb0604d42cbc8dbe9c
Ian Davies <ian.davies@anu.edu.au>
2020-03-29 11:26:23 +1100

    bug fix: initial waiting state

--- tw-core/src/au/edu/anu/twcore/experiment/Experiment.java
+++ tw-core/src/au/edu/anu/twcore/experiment/Experiment.java
@@ -107,7 +107,7 @@ public class Experiment
 				// TODO
 				controller = new StateMachineController(deployer);
 				// this puts the deployer in "waiting" state
-				controller.sendEvent(initialise.event());
+				//controller.sendEvent(initialise.event());
 			}
 			sealed = true;
 		}
