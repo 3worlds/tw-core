5f156c83824a044eac70029f0dc88ff1bae78647
Ian Davies <ian.davies@anu.edu.au>
2020-06-29 15:01:44 +1000

    Placeholder for RuntimeTreeData msgs

--- tw-core/src/au/edu/anu/twcore/data/runtime/RuntimeTreeData.java
+++ tw-core/src/au/edu/anu/twcore/data/runtime/RuntimeTreeData.java
@@ -2,13 +2,13 @@
  *  TW-CORE - 3Worlds Core classes and methods                            *
  *                                                                        *
  *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
- *       shayne.flint@anu.edu.au                                          *
+ *       shayne.flint@anu.edu.au                                          * 
  *       jacques.gignoux@upmc.fr                                          *
- *       ian.davies@anu.edu.au                                            *
+ *       ian.davies@anu.edu.au                                            * 
  *                                                                        *
  *  TW-CORE is a library of the principle components required by 3W       *
  *                                                                        *
- **************************************************************************
+ **************************************************************************                                       
  *  This file is part of TW-CORE (3Worlds Core).                          *
  *                                                                        *
  *  TW-CORE is free software: you can redistribute it and/or modify       *
@@ -19,36 +19,36 @@
  *  TW-CORE is distributed in the hope that it will be useful,            *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *  GNU General Public License for more details.                          *
+ *  GNU General Public License for more details.                          *                         
  *                                                                        *
  *  You should have received a copy of the GNU General Public License     *
  *  along with TW-CORE.                                                   *
  *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
  *                                                                        *
  **************************************************************************/
-package au.edu.anu.twcore.ecosystem.runtime.tracking;
+package au.edu.anu.twcore.data.runtime;
+
+import fr.cnrs.iees.twcore.constants.SimulatorStatus;
 
 /**
- * Records constants used for sending data messages
+ * @author Ian Davies
  *
- * @author Jacques Gignoux - 3 sept. 2019
- *
+ * @date 29 Jun 2020
  */
-public interface DataMessageTypes {
+public class RuntimeTreeData extends LabelledItemData {
 
-	public static final int MSGBASE = 1500;
+	public RuntimeTreeData(SimulatorStatus status, int senderId, int metaDataType) {
+		super(status, senderId, metaDataType);
+	}
 
-	public static final int METADATA = MSGBASE;
+	private Object somethingToLookAt;
 
-	public static final int VALUE_PAIR = MSGBASE + 1;
-	public static final int TIME_SERIES = VALUE_PAIR + 1;
-	public static final int TIME = TIME_SERIES + 1;// shouldn't this be DIM0?
-	public static final int DIM0 = TIME+1;
-	public static final int DIM1 = DIM0+1;
-	public static final int DIM2 = DIM1 + 1;// This can't go on to infinity - we need another way
-	public static final int SPACE = DIM2 + 1;
-	public static final int XY = SPACE + 1;
-	public static final int RTTree = XY + 1;
-	// etc...
+	public void setTree(Object somethingToLookAt) {
+		this.somethingToLookAt = somethingToLookAt;
+	}
+
+	public Object getTree() {
+		return somethingToLookAt;
+	}
 
 }
