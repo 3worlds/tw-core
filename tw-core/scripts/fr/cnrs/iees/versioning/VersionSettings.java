4d4dc42ce838615103087e2cebf8ec4233343663
Ian Davies <ian.davies@anu.edu.au>
2020-07-01 08:15:10 +1000

    version cleanup

--- tw-core/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-core/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -88,7 +88,7 @@ public class VersionSettings {
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.8,)", null},
 			{"fr.cnrs.iees.rvgrid", "rvgrid", "[0.0.3,)", null},
 			{"fr.cnrs.iees.uit", "uit", "[0.2.0,)", null},
-			{"org.apache.commons", "commons-text", "1.+", null},
+			{"org.apache.commons", "commons-text", "[1,)", null},
 			{"org.apache.ivy", "ivy", "2.2.0", null},
 			{"org.apache.odftoolkit","simple-odf","0.8.2-incubating", null},
 			{"com.hp.hpl.jena","jena","2.6.4", null},
