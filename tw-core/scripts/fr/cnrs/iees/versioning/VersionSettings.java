186dc7a04e2468c57c9b0221b789066c2555312f
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-07-01 16:52:49 +0200

    code + version cleanup

--- tw-core/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-core/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -83,12 +83,12 @@ public class VersionSettings {
 	 */
 	protected static String[][] DEPS = {
 			{"fr.ens.biologie", "generics", "[0.1.17,)", null},
-			{"au.edu.anu.rscs.aot", "omugi", "[0.2.16,)", null},
+			{"au.edu.anu.rscs.aot", "omugi", "[0.2.17,)", null},
 			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.3,)", null},
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.8,)", null},
 			{"fr.cnrs.iees.rvgrid", "rvgrid", "[0.0.3,)", null},
 			{"fr.cnrs.iees.uit", "uit", "[0.2.0,)", null},
-			{"org.apache.commons", "commons-text", "1.+", null},
+			{"org.apache.commons", "commons-text", "[1,)", null},
 			{"org.apache.ivy", "ivy", "2.2.0", null},
 			{"org.apache.odftoolkit","simple-odf","0.8.2-incubating", null},
 			{"com.hp.hpl.jena","jena","2.6.4", null},
