12e26864ed9484d47682177043a8b18779e933ba
Ian Davies <ian.davies@anu.edu.au>
2020-07-10 11:54:44 +1000

    bug fix: DataTrackerNode

--- tw-core/src/au/edu/anu/twcore/ecosystem/dynamics/DataTrackerNode.java
+++ tw-core/src/au/edu/anu/twcore/ecosystem/dynamics/DataTrackerNode.java
@@ -494,12 +494,17 @@ public class DataTrackerNode extends InitialisableNode
 			else if (etype instanceof Component) {
 				List<? extends CategorizedComponent<?>> cp = ((Component)etype).getInstance(index);
 				if (!cp.isEmpty()) {
+//					System.out.println(cp instanceof SystemComponent);
+//					System.out.println(cp.get(0) instanceof SystemComponent);
 					ls.addAll(cp);
+//					if (cp.get(0) instanceof SystemComponent)
+//						trackedContainer = ((SystemComponent) cp).container();
 					if (cp.get(0) instanceof SystemComponent)
-						trackedContainer = ((SystemComponent)cp).container();
+						trackedContainer = ((SystemComponent) cp.get(0)).container();
 					else if (cp.get(0) instanceof HierarchicalComponent)
 						// TODO: check this one
-						trackedContainer = ((HierarchicalComponent)cp).content().parentContainer();
+						trackedContainer = ((HierarchicalComponent) cp.get(0)).content().parentContainer();
+//					trackedContainer = ((HierarchicalComponent) cp).content().parentContainer();
 				}
 			}
 		}
