6910a78c6e6bc4aa2427f9db1ce61d4558d4a716
Ian Davies <ian.davies@anu.edu.au>
2020-09-24 14:51:44 +1000

    Clean up descendants of StringTable

--- tw-core/src/fr/cnrs/iees/twcore/constants/TrackerType.java
+++ tw-core/src/fr/cnrs/iees/twcore/constants/TrackerType.java
@@ -29,9 +29,7 @@
 
 package fr.cnrs.iees.twcore.constants;
 
-import static fr.cnrs.iees.io.parsing.TextGrammar.*;
 import au.edu.anu.rscs.aot.collections.tables.*;
-import fr.cnrs.iees.OmugiException;
 import fr.cnrs.iees.io.parsing.ValidPropertyTypes;
 
 /**
@@ -44,71 +42,13 @@ public class TrackerType extends StringTable {
 	public TrackerType(Dimensioner[] readDimensioners) {
 		super(readDimensioners);
 	}
-	//private static char QUOTE = '"';
-	// TODO IDD something needs to be done about this duplication
+
 	public static TrackerType valueOf(String value) {
-		if ((value==null)||value.isBlank()||value.isEmpty())
-			return null;
-		char[][] bdel = bdel();
-		char[] isep = isep();
-
-		String ss = TableAdapter.getBlockContent(value, bdel[TABLEix]);
-		String d = ss.substring(0, ss.indexOf(bdel[DIMix][BLOCK_CLOSE]) + 1);
-		TrackerType result = new TrackerType(readDimensioners(d, bdel[DIMix], isep[DIMix]));
-		ss = ss.substring(ss.indexOf(bdel[DIMix][BLOCK_CLOSE]) + 1);
-		StringBuilder sb = new StringBuilder();
-		int n = 0;
-		boolean inquote = false;
-		for (int i=0; i<ss.length(); i++) {
-			char c = ss.charAt(i);
-			if (c==DOUBLEQUOTE)
-				inquote = !inquote;
-			else if (inquote)
-				sb.append(c);
-			else if (!inquote) {
-				if (c==isep[TABLEix]) {
-					if (n==result.flatSize-1)
-						throw new OmugiException("Too many values read: table size == "+result.flatSize);
-					result.data[n++] = sb.toString().trim();
-					sb = new StringBuilder();
-				}
-				else
-					sb.append(c);
-			}
-		}
-		result.data[n++] = sb.toString().trim();
+		StringTable st = StringTable.valueOf(value);
+		TrackerType result = new TrackerType(st.getDimensioners());
+		for (int i = 0; i < st.size(); i++)
+			result.setWithFlatIndex(st.getWithFlatIndex(i), i);
 		return result;
-	}
-
-	private static char[][] bdel() {
-		char[][] result = new char[2][2];
-		result[Table.DIMix] = DIM_BLOCK_DELIMITERS;
-		result[Table.TABLEix] = TABLE_BLOCK_DELIMITERS;
-		return result;
-	}
-
-	private static char[] isep() {
-		char[] result = new char[2];
-		result[Table.DIMix] = DIM_ITEM_SEPARATOR;
-		result[Table.TABLEix] = TABLE_ITEM_SEPARATOR;
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		char[][] bdel = bdel();
-		char[] isep = isep();
-		StringBuilder sb = new StringBuilder(1024);
-		sb.append(bdel[TABLEix][BLOCK_OPEN]).append(bdel[DIMix][BLOCK_OPEN]).append(dimensioners[0].getLength());
-		for (int i = 1; i < dimensioners.length; i++)
-			sb.append(isep[DIMix]).append(dimensioners[i].getLength());
-		sb.append(bdel[DIMix][BLOCK_CLOSE]);
-		if (flatSize > 0)
-			sb.append(elementToString(0));
-		for (int i = 1; i < flatSize; i++)
-			sb.append(isep[TABLEix]).append(elementToString(i));
-		sb.append(bdel[TABLEix][BLOCK_CLOSE]);
-		return sb.toString();
 	}
 
 	public static TrackerType defaultValue() {
