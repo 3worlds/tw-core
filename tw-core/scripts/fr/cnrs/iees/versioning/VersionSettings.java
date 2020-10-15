19aee6efe22108ce7e844abec925e8d328f0c96d
Ian Davies <ian.davies@anu.edu.au>
2020-10-15 12:25:13 +1100

    version of rvgrid corrected (and remaining @Overrides

--- tw-core/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-core/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -86,7 +86,7 @@ public class VersionSettings {
 			{"au.edu.anu.rscs.aot", "omugi", "[0.4.0,)", null},
 			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.3,)", null},
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.8,)", null},
-			{"fr.cnrs.iees.rvgrid", "rvgrid", "[0.0.4,)", null},
+			{"fr.cnrs.iees.rvgrid", "rvgrid", "[0.0.3,)", null},
 			{"fr.cnrs.iees.uit", "uit", "[0.3.0,)", null},
 			{"org.apache.commons", "commons-text", "[1,)", null},
 			// removed as twsetup is now in separate lib.
