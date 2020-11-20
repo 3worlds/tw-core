e41697ce0a62dfd505f00e81a1e89895a2528f6b
Ian Davies <ian.davies@anu.edu.au>
2020-11-20 13:11:46 +1100

    package-info text edit

--- tw-core/src/au/edu/anu/twcore/archetype/tw/package-info.java
+++ tw-core/src/au/edu/anu/twcore/archetype/tw/package-info.java
@@ -3,20 +3,30 @@
  */
 /**
  *
- * <p>This package contains (1) all the <strong>archetypes</strong> (*.ugt) files that define the specification of a model in
- * 3Worlds; and (2) all the {@code Query} subclasses required by the previous files to define constraints on
- * specifications.</p>
+ * <p>
+ * This package contains: (1) all the <strong>archetype</strong> (*.ugt) files
+ * that define the specification of a model in 3Worlds; and (2) all the
+ * {@code Query} subclasses required by these archetype files to define
+ * constraints on specifications.
+ * </p>
  *
- * <p><strong>For developers: about Query scope.</strong> constraint Queries tend to generate a lot of warning or error messages which may
- * overwhelm the naive reader with difficult-to-decipher information. To maximize the usefulness
- * of these message, one should be careful to only report what the query is effectively testing
- * (remember that any exception will be caught and reported to the log window by the checker). Also,
- * notice that the use in a Query of the CoreQueries.get(...) method may generate many messages in
- * the context of the ModelMaker graph editor, where the being constructed graph can be in any state,
- * most of the time invalid. When another constraint is not fulfilled that prevents the Query to
- * make a decision, it should be mute, as the problem will be handled by another Query. In particular,
- * Avoid triggering a lower level query by <em>not</em> using oneOrMany: use zeroOrMany
- * instead.
+ * <p>
+ * <strong>For developers: about Query scope.</strong> Constraint Queries can be
+ * overly verbose at times and make interpretation of the error difficult to
+ * decipher. To maximize the usefulness of these messages, take care to only
+ * report what the query is effectively testing (remember that any exception
+ * will be caught and reported to the log window by the checker). Also, notice
+ * that using the query system within the body of a query (e.g by calling
+ * CoreQueries.get(...)) risks generating additional error messages if that
+ * query fails for some reason. Therefore, if using queries within a query, make
+ * sure the queries you call cannot fail. Remember the graph being tested may be
+ * in any state during editing. When another constraint is not fulfilled that
+ * prevents the Query from making a decision, it should remain mute (i.e
+ * satisfied), as the problem will be handled by another Query. For example, if
+ * your query uses selectOneOrMany(...) and there are none of what you are
+ * searching for, an exception will be generated for the selectOneOrMany() query
+ * itself. Instead, consider using selectZeroOrMany() and handle the case of the
+ * empty list within your query.
  * </p>
  *
  * @author J. Gignoux & I.D. Davies - 19 nov. 2020
