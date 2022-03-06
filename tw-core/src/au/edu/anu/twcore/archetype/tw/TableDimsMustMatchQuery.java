69f659d2c60191fea78025a7e2b4a6a3c536ac2a
Ian Davies <ian.davies@anu.edu.au>
2022-03-07 08:31:33 +1100

    Table size mismatch query

---
+++ tw-core/src/au/edu/anu/twcore/archetype/tw/TableDimsMustMatchQuery.java
@@ -0,0 +1,82 @@
+/**************************************************************************
+ *  TW-CORE - 3Worlds Core classes and methods                            *
+ *                                                                        *
+ *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          * 
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  TW-CORE is a library of the principle components required by 3W       *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of TW-CORE (3Worlds Core).                          *
+ *                                                                        *
+ *  TW-CORE is free software: you can redistribute it and/or modify       *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  TW-CORE is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with TW-CORE.                                                   *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
+ *                                                                        *
+ **************************************************************************/
+package au.edu.anu.twcore.archetype.tw;
+
+import au.edu.anu.rscs.aot.collections.tables.StringTable;
+import au.edu.anu.rscs.aot.queries.QueryAdaptor;
+import au.edu.anu.rscs.aot.queries.Queryable;
+import au.edu.anu.twcore.TextTranslations;
+import fr.cnrs.iees.graph.DataHolder;
+import fr.cnrs.iees.graph.ReadOnlyDataHolder;
+
+/**
+ * @author Ian Davies
+ *
+ * @date 7 Mar 2022
+ */
+
+/**
+ * Constraint: String tables of given property keys must have the same
+ * dimensions.
+ * 
+ * Input is dataHolder
+ */
+public class TableDimsMustMatchQuery extends QueryAdaptor {
+	private String key1;
+	private String key2;
+
+	public TableDimsMustMatchQuery(StringTable args) {
+		key1 = args.getWithFlatIndex(0);
+		key2 = args.getWithFlatIndex(1);
+	}
+
+	@Override
+	public Queryable submit(Object input) {
+		initInput(input);
+		DataHolder dh = (DataHolder) input;
+		StringTable t1;
+		StringTable t2;
+		if (dh.properties().hasProperty(key1))
+			t1 = (StringTable) dh.properties().getPropertyValue(key1);
+		else
+			return this;
+		if (dh.properties().hasProperty(key2))
+			t2 = (StringTable) dh.properties().getPropertyValue(key2);
+		else
+			return this;
+		if (!t1.sameDimensionsAs(t2)){
+			String[] msgs = TextTranslations.getTableDimsMustMatch(key1,key2);
+			actionMsg = msgs[0];
+			errorMsg = msgs[1];
+		}
+		
+		return this;
+	}
+
+}
