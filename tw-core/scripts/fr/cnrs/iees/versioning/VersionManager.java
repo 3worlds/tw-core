01e6304de0399e73d0364c66f103230bc553b582
Jacques Gignoux <jacques.gignoux@upmc.fr>
2022-08-25 10:51:37 +0200

    fixed javadoc location in VersionManager

--- tw-core/scripts/fr/cnrs/iees/versioning/VersionManager.java
+++ tw-core/scripts/fr/cnrs/iees/versioning/VersionManager.java
@@ -1,31 +1,3 @@
-/**************************************************************************
- *  TW-CORE - 3Worlds Core classes and methods                            *
- *                                                                        *
- *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
- *       shayne.flint@anu.edu.au                                          * 
- *       jacques.gignoux@upmc.fr                                          *
- *       ian.davies@anu.edu.au                                            * 
- *                                                                        *
- *  TW-CORE is a library of the principle components required by 3W       *
- *                                                                        *
- **************************************************************************                                       
- *  This file is part of TW-CORE (3Worlds Core).                          *
- *                                                                        *
- *  TW-CORE is free software: you can redistribute it and/or modify       *
- *  it under the terms of the GNU General Public License as published by  *
- *  the Free Software Foundation, either version 3 of the License, or     *
- *  (at your option) any later version.                                   *
- *                                                                        *
- *  TW-CORE is distributed in the hope that it will be useful,            *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *  GNU General Public License for more details.                          *                         
- *                                                                        *
- *  You should have received a copy of the GNU General Public License     *
- *  along with TW-CORE.                                                   *
- *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
- *                                                                        *
- **************************************************************************/
 package fr.cnrs.iees.versioning;
 
 /**********************************************************************************************
@@ -125,7 +97,7 @@ public class VersionManager {
 
 	"\t<!-- Artifacts are produced in the project lib directory-->\n" +
 	"\t<property name=\"jarRepo\" location=\"${user.dir}/../lib\"/>\n\n" +
-	"\t<property name=\"docRepo\" location=\"${user.dir}/../doc/javadoc\"/>" +
+	"\t<property name=\"docRepo\" location=\"${user.dir}/../javadoc\"/>" +
 
 	"\t<!-- Use this string as the organisation package -->\n" +
 	"\t<property name=\"org\" value=\"" + ORG +"\"/>\n\n" +
